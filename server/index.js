0b6afbb44b470ee5cd2eb478eb6904613696b286
dli9431 <dli9431@gmail.com>
2021-06-23 15:42:02 -0700

    google oauth test

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
+const {google} = require('googleapis');
 const express = require('express');
 const path = require('path');
+const { Server } = require('http');
+
+const client = new google.auth.OAuth2(
+	process.env.google_client_id,
+	process.env.google_client_secret
+)
+
+const scopes = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile', 'openid', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets'];
+
+this.authorizeUrl = client.generateAuthUrl({
+	access_type: 'offline',
+	scope: scopes,
+});
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,10 +24,27 @@ app.get('/api', (req, res) => {
 	res.json({ message: 'Hello from server!' });
 });
 
+app.get('/oauth2callback', (req, res) => {
+	const code = req.query.code;
+	client.getToken(code, (err, tokens) => {
+		if (err) {
+			console.error('Error getting oAuth tokens: ');
+			throw err;
+		}
+		client.credentials = tokens;
+		res.send('Authentication successful! Please return to the console.');
+		server.close();
+	})
+});
+
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-	console.log(`server listening on ${PORT}`);
+const server = app.listen(PORT, () => {
+	open(this.authorizeUrl, {wait: false});
 });
+
+// app.listen(PORT, () => {
+// 	console.log(`server listening on ${PORT}`);
+// });
